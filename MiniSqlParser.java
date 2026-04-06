51c9842be3cb58d9712315d49e087b5aaf890a44
tanzilahmed0 <tanahmed03@gmail.com>
2026-04-06 15:25:24 -0700

    completed lexer

--- MiniSqlParser.java
+++ MiniSqlParser.java
@@ -146,8 +146,86 @@ public class MiniSqlParser {
 
   // ===== Lexer =====
   static List<Token> tokenize(String sql) {
-    // TODO: implement tokenizer
-    return List.of(new Token(TokenType.EOF, "EOF"));
+    List<Token> tokens = new ArrayList<>();
+    int i = 0, n = sql.length();
+
+    while (i < n) {
+      char c = sql.charAt(i);
+
+      // whitespace
+      if (Character.isWhitespace(c)) { i++; continue; }
+
+      // string literal '...' with '' as escaped quote
+      if (c == '\'') {
+        StringBuilder sb = new StringBuilder();
+        i++; // skip opening quote
+        while (i < n) {
+          if (sql.charAt(i) == '\'') {
+            if (i + 1 < n && sql.charAt(i + 1) == '\'') { sb.append('\''); i += 2; }
+            else { i++; break; } // closing quote
+          } else {
+            sb.append(sql.charAt(i++));
+          }
+        }
+        tokens.add(new Token(TokenType.STRING, sb.toString()));
+        continue;
+      }
+
+      // number: 123, 3.5, .5
+      if (Character.isDigit(c) || (c == '.' && i + 1 < n && Character.isDigit(sql.charAt(i + 1)))) {
+        int start = i;
+        while (i < n && Character.isDigit(sql.charAt(i))) i++;
+        if (i < n && sql.charAt(i) == '.') {
+          i++;
+          while (i < n && Character.isDigit(sql.charAt(i))) i++;
+        }
+        tokens.add(new Token(TokenType.NUMBER, sql.substring(start, i)));
+        continue;
+      }
+
+      // identifier or keyword (case-insensitive keyword match)
+      if (Character.isLetter(c) || c == '_') {
+        int start = i;
+        while (i < n && (Character.isLetterOrDigit(sql.charAt(i)) || sql.charAt(i) == '_')) i++;
+        String word = sql.substring(start, i);
+        String upper = word.toUpperCase();
+        TokenType type;
+        if      (upper.equals("SELECT"))  type = TokenType.SELECT;
+        else if (upper.equals("FROM"))    type = TokenType.FROM;
+        else if (upper.equals("WHERE"))   type = TokenType.WHERE;
+        else if (upper.equals("AND"))     type = TokenType.AND;
+        else if (upper.equals("OR"))      type = TokenType.OR;
+        else if (upper.equals("NOT"))     type = TokenType.NOT;
+        else if (upper.equals("BETWEEN")) type = TokenType.BETWEEN;
+        else                              type = TokenType.IDENT;
+        tokens.add(new Token(type, word));
+        continue;
+      }
+
+      // two-character operators
+      if (i + 1 < n) {
+        String two = sql.substring(i, i + 2);
+        if (two.equals("!=") || two.equals("<>")) { tokens.add(new Token(TokenType.NE, two)); i += 2; continue; }
+        if (two.equals("<="))                      { tokens.add(new Token(TokenType.LE, two)); i += 2; continue; }
+        if (two.equals(">="))                      { tokens.add(new Token(TokenType.GE, two)); i += 2; continue; }
+      }
+
+      // single-character tokens
+      if      (c == '=') { tokens.add(new Token(TokenType.EQ,     "=")); i++; continue; }
+      else if (c == '<') { tokens.add(new Token(TokenType.LT,     "<")); i++; continue; }
+      else if (c == '>') { tokens.add(new Token(TokenType.GT,     ">")); i++; continue; }
+      else if (c == ',') { tokens.add(new Token(TokenType.COMMA,  ",")); i++; continue; }
+      else if (c == '.') { tokens.add(new Token(TokenType.DOT,    ".")); i++; continue; }
+      else if (c == '(') { tokens.add(new Token(TokenType.LPAREN, "(")); i++; continue; }
+      else if (c == ')') { tokens.add(new Token(TokenType.RPAREN, ")")); i++; continue; }
+      else if (c == '*') { tokens.add(new Token(TokenType.STAR,   "*")); i++; continue; }
+      else if (c == ';') { tokens.add(new Token(TokenType.SEMI,   ";")); i++; continue; }
+
+      throw new RuntimeException("Unexpected character '" + c + "' at position " + i);
+    }
+
+    tokens.add(new Token(TokenType.EOF, ""));
+    return tokens;
   }
 
   // ===== Parser =====
@@ -172,24 +250,14 @@ public class MiniSqlParser {
   }
 
   public static void main(String[] args) {
-    // Manually-built AST for:
-    // SELECT * FROM student s WHERE (gpa >= 3.5 AND name = 'it''s ok') OR NOT (gpa < .5);
-    AstNode where = new OrNode(
-      new AndNode(
-        new CompareNode(">=", new AttrNode(null, "gpa"), new NumberNode("3.5")),
-        new CompareNode("=",  new AttrNode(null, "name"), new StringNode("it's ok"))
-      ),
-      new NotNode(
-        new CompareNode("<", new AttrNode(null, "gpa"), new NumberNode(".5"))
-      )
-    );
-
-    QueryNode q = new QueryNode(
-      new SelectListNode(true, List.of()),
-      new FromListNode(List.of(new TableNode("student", "s"))),
-      where
-    );
-
+    String sql = """
+      SELECT *
+      FROM student s
+      WHERE (gpa >= 3.5 AND name = 'it''s ok') OR NOT (gpa < .5);
+      """;
+    List<Token> toks = tokenize(sql);
+    Parser p = new Parser(toks);
+    QueryNode q = p.parseQuery();
     q.print("");
   }
 }
